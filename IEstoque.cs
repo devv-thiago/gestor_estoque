1f77341eea007a8f41688c59131b5a527e0af557
devv-thiago <tfofano3@gmail.com>
2023-09-08 09:02:08 -0300

    implementando classe de produto e interface de estoque

---
+++ IEstoque.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestor_estoque
+{
+    internal interface IEstoque
+    {
+        void adicionarProduto();
+        void listarProduto();
+        void atualizarProduto();
+        void excluirProduto();
+
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestor_estoque
+{
+    abstract class Produto : IEstoque
+    {
+        private static int proximoId = 1;
+        private int id;
+        private string nomeProduto;
+        string descricao;
+        double preco;
+        int quantidadeEstoque;
+
+
+        protected Produto(string nome, string descricao, double preco, int quantidadeEstoque)
+        {
+            this.id = proximoId++;
+            this.NomeProduto = nome;
+            this.Descricao = descricao;
+            this.Preco = preco;
+            this.QuantidadeEstoque = quantidadeEstoque;
+        }
+
+        public int QuantidadeEstoque { get => quantidadeEstoque; set => quantidadeEstoque = value; }
+        public int Id { get => id; }
+        public string NomeProduto { get => nomeProduto; set => nomeProduto = value; }
+        public double Preco { get => preco; set => preco = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+
+        public void adicionarProduto()
+        {
+
+        }
+
+        public void atualizarProduto()
+        {
+
+        }
+
+        public void excluirProduto()
+        {
+
+        }
+
+        public void listarProdutoNome()
+        {
+
+        }
+
+        public void listarProdutos()
+        {
+
+        }
+    }
+}
+
+
